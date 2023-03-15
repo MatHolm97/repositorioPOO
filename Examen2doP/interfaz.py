ffc34eb5db7c0a97ef2c99826e27de1857c64d67
Chiapas <122493102+Chiapas@users.noreply.github.com>
2023-03-15 17:10:37 -0600

    Examenn 2.2
    Segunda parte

--- Examen2doP/interfaz.py
+++ Examen2doP/interfaz.py
@@ -5,32 +5,35 @@ from estudiante import Estudiante
 class Interfaz:
     def __init__(self, master):
         self.master = master
-        master.title("Generador de matrículas")
+        master.title("Generador de matrículas" )
 
-        # Crear etiquetas
-        tk.Label(master, text="Nombre:").grid(row=0)
-        tk.Label(master, text="Apellido Paterno:").grid(row=1)
-        tk.Label(master, text="Apellido Materno:").grid(row=2)
-        tk.Label(master, text="Año de nacimiento:").grid(row=3)
-        tk.Label(master, text="Carrera:").grid(row=4)
+        
+        tk.Label(master, text="Nombre:" , bg="orange").grid(row=0)
+        tk.Label(master, text="Apellido Paterno:" , bg="orange").grid(row=1)
+        tk.Label(master, text="Apellido Materno:" , bg="orange").grid(row=2)
+        tk.Label(master, text="Año de nacimiento:" , bg="orange").grid(row=3)
+        tk.Label(master, text="Carrera:" , bg="orange").grid(row=4)
+        tk.Label(master, text="Año de curso:" , bg="orange").grid(row=5)
 
-        # Crear campos de entrada
+        
         self.entry_nombre = tk.Entry(master)
         self.entry_apellido_paterno = tk.Entry(master)
         self.entry_apellido_materno = tk.Entry(master)
         self.entry_año_nacimiento = tk.Entry(master)
         self.entry_carrera = tk.Entry(master)
+        self.entry_año_curso = tk.Entry(master)
 
-        # Colocar campos de entrada
+       
         self.entry_nombre.grid(row=0, column=1)
         self.entry_apellido_paterno.grid(row=1, column=1)
         self.entry_apellido_materno.grid(row=2, column=1)
         self.entry_año_nacimiento.grid(row=3, column=1)
-        self.entry_carrera.grid(row=4, column=1)
+        self.entry_carrera.grid(row=5, column=1)
+        self.entry_año_curso.grid(row=4, column=1)
 
-        # Crear botón
-        self.button_generar_matricula = tk.Button(master, text="Generar matrícula", command=self.generar_matricula)
-        self.button_generar_matricula.grid(row=5, columnspan=2)
+       
+        self.button_generar_matricula = tk.Button(master, text="Generar matrícula",   fg="green" , command=self.generar_matricula)
+        self.button_generar_matricula.grid(row=6, columnspan=2)
 
     def generar_matricula(self):
         nombre = self.entry_nombre.get()
@@ -38,11 +41,12 @@ class Interfaz:
         apellido_materno = self.entry_apellido_materno.get()
         año_nacimiento = int(self.entry_año_nacimiento.get())
         carrera = self.entry_carrera.get()
+        año_curso = int(self.entry_año_curso.get())
 
-        estudiante = Estudiante(nombre, apellido_paterno, apellido_materno, año_nacimiento, carrera)
+        estudiante = Estudiante(nombre, apellido_paterno, apellido_materno, año_nacimiento, carrera , año_curso)
         matricula = estudiante.generar_matricula()
 
-        messagebox.showinfo("Matrícula generada", f"La matrícula generada es: {matricula}")
+        messagebox.showinfo("Matricula generada", f"La matricula generada es: {matricula}")
         
     
 
---
+++ Examen2doP/main.py
@@ -0,0 +1,10 @@
+
+import tkinter as tk
+from interfaz import Interfaz
+
+# dale play pa 
+
+
+root = tk.Tk()
+app = Interfaz(root)
+root.mainloop()
