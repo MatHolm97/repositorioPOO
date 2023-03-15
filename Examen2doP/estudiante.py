c405db9e3252d8ec2f624970d3e5ee8cdca6e0cd
Chiapas <122493102+Chiapas@users.noreply.github.com>
2023-03-15 17:17:05 -0600

    Examen 2
    Final 3

--- Examen2doP/estudiante.py
+++ Examen2doP/estudiante.py
@@ -1,18 +1,18 @@
 import random
 
 class Estudiante:
-    def __init__(self, nombre, apellido_paterno, apellido_materno, año_nacimiento, carrera , año_curso):
+    def __init__(self, nombre, apellido_paterno, apellido_materno, año_nacimiento, carrera ):
         self.nombre = nombre
         self.apellido_paterno = apellido_paterno
         self.apellido_materno = apellido_materno
         self.año_nacimiento = año_nacimiento
         self.carrera = carrera
-        self.año_curso = año_curso
+        
 
     def generar_matricula(self):
        
         año_nacimiento = str(self.año_nacimiento)[-2:]
-        año_curso = str(self.año_curso)[-2:]
+        
         primera_letra_nombre = self.nombre[0]
         tres_letras_apellido_paterno = self.apellido_paterno[:3]
         tres_letras_apellido_materno = self.apellido_materno[:3]
@@ -21,6 +21,6 @@ class Estudiante:
 
         #matricula = año_actual + año_nacimiento + primera_letra_nombre + tres_letras_apellido_paterno + tres_letras_apellido_materno + tres_digitos_aleatorios + tres_primeras_letras_carrera 
 
-        matricula = tres_primeras_letras_carrera + año_curso + año_nacimiento + primera_letra_nombre + tres_letras_apellido_paterno + tres_letras_apellido_materno + tres_digitos_aleatorios
+        matricula = tres_primeras_letras_carrera  + año_nacimiento + primera_letra_nombre + tres_letras_apellido_paterno + tres_letras_apellido_materno + tres_digitos_aleatorios
         
         return matricula
--- Examen2doP/interfaz.py
+++ Examen2doP/interfaz.py
@@ -4,7 +4,7 @@ from estudiante import Estudiante
 
 class Interfaz:
     def __init__(self, master):
-        self.master = master
+        self.master = master 
         master.title("Generador de matrículas" )
 
         
@@ -13,7 +13,7 @@ class Interfaz:
         tk.Label(master, text="Apellido Materno:" , bg="orange").grid(row=2)
         tk.Label(master, text="Año de nacimiento:" , bg="orange").grid(row=3)
         tk.Label(master, text="Carrera:" , bg="orange").grid(row=4)
-        tk.Label(master, text="Año de curso:" , bg="orange").grid(row=5)
+        
 
         
         self.entry_nombre = tk.Entry(master)
@@ -21,19 +21,18 @@ class Interfaz:
         self.entry_apellido_materno = tk.Entry(master)
         self.entry_año_nacimiento = tk.Entry(master)
         self.entry_carrera = tk.Entry(master)
-        self.entry_año_curso = tk.Entry(master)
-
+        
        
         self.entry_nombre.grid(row=0, column=1)
         self.entry_apellido_paterno.grid(row=1, column=1)
         self.entry_apellido_materno.grid(row=2, column=1)
         self.entry_año_nacimiento.grid(row=3, column=1)
-        self.entry_carrera.grid(row=5, column=1)
-        self.entry_año_curso.grid(row=4, column=1)
+        self.entry_carrera.grid(row=4, column=1)
+        
 
        
         self.button_generar_matricula = tk.Button(master, text="Generar matrícula",   fg="green" , command=self.generar_matricula)
-        self.button_generar_matricula.grid(row=6, columnspan=2)
+        self.button_generar_matricula.grid(row=5, columnspan=2)
 
     def generar_matricula(self):
         nombre = self.entry_nombre.get()
@@ -41,9 +40,9 @@ class Interfaz:
         apellido_materno = self.entry_apellido_materno.get()
         año_nacimiento = int(self.entry_año_nacimiento.get())
         carrera = self.entry_carrera.get()
-        año_curso = int(self.entry_año_curso.get())
+        
 
-        estudiante = Estudiante(nombre, apellido_paterno, apellido_materno, año_nacimiento, carrera , año_curso)
+        estudiante = Estudiante(nombre, apellido_paterno, apellido_materno, año_nacimiento, carrera )
         matricula = estudiante.generar_matricula()
 
         messagebox.showinfo("Matricula generada", f"La matricula generada es: {matricula}")
