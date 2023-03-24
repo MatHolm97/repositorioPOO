85575f91bdef640c21db2f750ae8ced004018af9
Chiapas <122493102+Chiapas@users.noreply.github.com>
2023-03-24 17:12:04 -0600

    UsuariosCRUD
    Encriptar la contraseña por medio de bcrypt y sal

--- tkintherSql/controladorBD.py
+++ tkintherSql/controladorBD.py
@@ -1,3 +1,45 @@
 from tkinter import messagebox
 import sqlite3
 import bcrypt
+
+class controladorBD:
+    
+    def __init(self):
+        pass
+    
+    def conexionBD(self):
+        try: 
+            conexion= sqlite3.connect("/Users/matiashernandezdelolmo/Downloads/5CuatriUpq/POO/repositorio/DBusuarios.db")
+            print("Conectado ")
+            return conexion
+        except sqlite3.OperationalError:
+            print("No se pudo conectar")
+            
+            
+    def guardarUsuario(self,nom,cor,con):
+        
+        conx= self.conexionBD()
+        
+        if(nom == "" or cor == "" or con == ""):
+            messagebox.showwarning("Aguas","Revisa tus datos")
+            conx.close()
+        else:
+            cursor= conx.cursor()
+            conH= self.encriptarCon(con)
+            datos=(nom,cor,conH)
+            qrInsert="insert into TBRegistrados(nombre,correo,contra) values(?,?,?)" 
+            
+            
+            cursor.execute(qrInsert,datos)
+            conx.commit()
+            conx.close()
+            messagebox.showinfo("Exito", "Exitoso")  
+            
+    
+    def encriptarCon(self,con):
+        conPlana= con 
+        conPlana= conPlana.encode()
+        sal= bcrypt.gensalt()
+        conHa= bcrypt.hashpw(conPlana,sal)
+        print(conHa)
+        return conHa  
